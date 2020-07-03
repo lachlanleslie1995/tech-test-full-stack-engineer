df36b4a9c7e9fa7b91d66469f2d325f7f61bf3b0
lachlanleslie1995 <lachlan.leslie1995@gmail.com>
2020-07-03 17:55:48 +1000

    Add in extra route to be able to query landing pads, still requires integration with database

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const dbPool = require("./db");
 const express = require("express");
 const bodyParser = require("body-parser");
-const { getCapsules } = require("./resources/spacexRequests");
+const { getCapsules, getLandingPads } = require("./resources/spacexRequests");
 
 const app = express();
 
@@ -16,21 +16,55 @@ app.get("/api/v1/capsule", async (req, res) => {
       result: response.data,
     });
   } catch (e) {
-    res.status(400);
+    res.status(404);
     res.send({
       error: "Error:" + e,
-      errorMsg: "Failed to get capsule results",
+      errorMsg: "Failed to get capsules",
+      url: req.url,
+    });
+  }
+});
+
+app.get("/api/v1/landingpad/:id", async (req, res) => {
+  let id = req.params.id;
+  const rows = await dbPool.query("SELECT * FROM spaceData");
+  let result = "";
+
+  try {
+    if (rows.length) {
+    } else {
+      let response = await getLandingPads(id);
+      if (response.data) {
+        result = res.status(200);
+        result = {
+          id: response.data.id,
+          full_name: response.data.full_name,
+          status: response.data.status,
+          location: response.data.location,
+        };
+        await dbPool.query(
+          `INSERT INTO spaceData (id, spaceItem) VALUES('?, ?')`,
+          [result.id, JSON.stringify(result)]
+        );
+      }
+    }
+    res.send({
+      result: result,
+    });
+  } catch (e) {
+    res.status(404);
+    res.send({
+      error: "" + e,
+      errorMsg: "Failed to get Landing Pad",
       url: req.url,
     });
   }
 });
 
 app.get("/", async (req, res) => {
-  //   const rows = await dbPool.query("SELECT * FROM spaceData");
   res.status(200);
   res.send({
-    // result: JSON.stringify(rows),
-    result: "boo",
+    result: "Welcome to Space airport API",
   });
 });
 
--- server/resources/spacexRequests.js
+++ server/resources/spacexRequests.js
@@ -2,9 +2,11 @@ const axios = require("axios");
 
 const getCapsules = async () => {
   return axios.get("https://api.spacexdata.com/v3/capsules");
-  //     axios.get("https://api.spacexdata.com/v3/capsules").then((response) => {
-  //     return JSON.stringify(response.data);
-  //   });
+};
+
+const getLandingPads = async (id) => {
+  return axios.get(`https://api.spacexdata.com/v3/landpads/${id}`);
 };
 
 exports.getCapsules = getCapsules;
+exports.getLandingPads = getLandingPads;
